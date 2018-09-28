0e4f80af0d9c08a0ab2f93ca49f3a3eb07783495
Eduardo Dantas Nunes <eduardo.dnunes@hotmail.com>
2018-09-28 17:16:12 -0300

    Criação da API.

---
+++ ApiTeste20180928/ApiTeste20180928/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTeste20180928.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Você digitou o número {id}.";
+        }
+    }
+}
